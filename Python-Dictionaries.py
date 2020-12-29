9f05bd578987e64786e78baa0b77507c81edf5a5
GesangPJ <g.paudra@gmail.com>
2020-12-29 21:44:11 +0700

    Create Python-Dictionaries.py

---
+++ Python-Dictionaries.py
@@ -0,0 +1,32 @@
+#Dictionaries
+#Dictionaries (Kamus) adalah suatu set dalam python, dictionaries terdiri dari :
+# <Nama Kamus> = ('key' : 'value' , 'key' : 'value", .... 'key -n' : 'value -n')
+
+#Membuat dictionary kamus1
+kamus1={'barang1':80000, 'barang2':50000}
+
+#print kamus1
+kamus1 
+
+#Data01
+Data01={'Nama':'Paijo', 'umur':20}
+Data01 
+
+#Complex Dictionary
+kompleks={'dd01':909,'dd02':[3,4,5],'dd3':{'KunciDalam':100}}
+kompleks['dd02']
+
+kompleks['dd02'][2]
+
+kompleks['dd3']
+
+kompleks['dd3']['KunciDalam']
+
+#Complex 2
+komplex={'kunci1':['r','s','t']}
+
+komplex
+
+#Modifikasi Dictionary
+
+
