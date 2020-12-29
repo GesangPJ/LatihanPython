bc776b00187bb3891aa50bf667afcd5b4dfda89f
GesangPJ <g.paudra@gmail.com>
2020-12-29 22:12:47 +0700

    Create Python-Sets.py

---
+++ Python-Sets.py
@@ -0,0 +1,17 @@
+#Sets adalah Koleksi elemen unik yang tidak berurutan
+
+#Membuat SETS
+setku = set()
+
+#Menambah isi Set
+setku.add(5)
+
+#melihat hasil
+setku
+
+#Contoh perbedaan SETS dengan list *nilai yang sama tidak ditambahkan kembali
+setku.add(8)
+
+daftarku=[7,7,7,7,8,8,8,8,9,9,9,9,0,0,0,0]
+
+set(daftarku)
